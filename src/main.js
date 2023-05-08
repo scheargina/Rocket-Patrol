522f20e45d9e14d1bb6c62fc6ea1f40f625f0e81
Jingqi Huang <dicskyowner@gmail.com>
2023-05-08 13:16:53 -0700

    Explanatory Comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
+/*
+Jingqi Huang
+Mod title:Rocket Patrol Refactor!
+The approximate time it took to complete the project：4h
+
+Mods chose from the list above
+Track a high score that persists across scenes and display it in the UI (5)
+Implement the 'FIRE' UI text from the original game (5)
+Add your own (copyright-free) background music to the Play scene (please be mindful of the volume) (5)
+Implement the speed increase that happens after 30 seconds in the original game (5)
+Create a new scrolling tile sprite for the background (5)
+Allow the player to control the Rocket after it's fired (5)
+
+Create 4 new explosion sound effects and randomize which one plays on impact (10)
+Display the time remaining (in seconds) on the screen (10)
+Using a texture atlas, create a new animated sprite for the Spaceship enemies (10)
+Create a new title screen (e.g., new artwork, typography, layout) (10)
+
+Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15)
+Implement a new timing/scoring mechanism that adds time to the clock for successful hits (15)
+
+citations for backgorund:
+https://pixabay.com/ */
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
